8cd38d1b44acaee0595a18b0846acb702cf07a80
Kquit <101361785+Kquit@users.noreply.github.com>
2022-03-10 22:42:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,37 @@
+import os
 import telebot
+from flask import Flask, request
+
+TOKEN = '1810242195:AAFfImqq9mPT81rs3ApL74eUob5u8A9oxb8'
+APP_URL = f'https://vitaljaheroku.herokuapp.com/{TOKEN}'
+bot = telebot.TeleBot(TOKEN)
+server = Flask(__name__)
 
 
-
-from telebot import types
-# pip install pytelegrambotapi
-token = '5280003455:AAGOqtbvUpLI_DL_HIbz8h4N8ocmJGFoYbc'
-bot = telebot.TeleBot(token)
-chat_id = '-1001667629231' # Например chat_id = '223344'
-# Telegram bot: Get My Id
-
-@bot.message_handler(commands = ['start'])
-def main(message):
-    bot.send_message(message.chat.id, text="Привет,что бы записаться на тренировку пожалуйста оставь заявку в формате:")
-    bot.send_message(message.chat.id, text="Имя Фамилия,жанр,дата-время")
-
-    markup = types.InlineKeyboardMarkup()
-    btn_my_site= types.InlineKeyboardButton(text='Расписание', url='https://aerialrope.fun/go')
-    markup.add(btn_my_site)
-    bot.send_message(message.chat.id, "Время и дату тренировок можно узнать тут 👇", reply_markup = markup)
-
-    bot.send_message(message.chat.id, text="Напрмиер: Павел Иванов,акробатика 15.02 16:30")
-    bot.send_message(message.chat.id, text="После,вы будете автоматически записаны на тренировку")
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
 
 
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo(message):
+    bot.reply_to(message, message.text)
 
-@bot.message_handler(content_types=['text'])
-def all_messages(message):
-    bot.forward_message(chat_id, message.chat.id, message.message_id)
+
+@server.route('/' + TOKEN, methods=['POST'])
+def get_message():
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return '!', 200
+
+
+@server.route('/')
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url=APP_URL)
+    return '!', 200
+
 
 if __name__ == '__main__':
-    bot.polling(none_stop=True)
+    server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
